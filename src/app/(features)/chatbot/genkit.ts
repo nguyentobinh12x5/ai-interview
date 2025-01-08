9de82fedbd7e030e2fb1378adc8fdb067bd85478
binh.nguyento <nguyentobinh12@gmail.com>
2025-01-08 17:34:01 +0700

    genkit

--- src/app/(features)/chatbot/genkit.ts
+++ src/app/(features)/chatbot/genkit.ts
@@ -1,9 +1,10 @@
 "use server";
 import { gemini15Flash, googleAI } from "@genkit-ai/googleai";
-import { genkit, z } from "genkit";
+import { genkit, run, z } from "genkit";
 import {
   devLocalIndexerRef,
   devLocalVectorstore,
+  devLocalRetrieverRef,
 } from "@genkit-ai/dev-local-vectorstore";
 import { textEmbedding004, vertexAI } from "@genkit-ai/vertexai";
 import { Document } from "genkit/retriever";
@@ -11,6 +12,8 @@ import { chunk } from "llm-chunk";
 import { readFile } from "fs/promises";
 import path from "path";
 import pdf from "pdf-parse";
+const menuPdfIndexer = devLocalIndexerRef("menuQA");
+const menuRetriever = devLocalRetrieverRef("menuQA");
 
 async function extractTextFromPdf(filePath: string) {
   const pdfFile = path.resolve(filePath);
@@ -18,6 +21,54 @@ async function extractTextFromPdf(filePath: string) {
   const data = await pdf(dataBuffer);
   return data.text;
 }
+
+const chunkingConfig = {
+  minLength: 1000,
+  maxLength: 2000,
+  splitter: "sentence",
+  overlap: 100,
+  delimiters: "",
+} as any;
+
+const MenuItemSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  calories: z.number(),
+  allergens: z.array(z.string()),
+});
+
+const ai = genkit({
+  plugins: [
+    googleAI(),
+    // vertexAI provides the textEmbedding004 embedder
+    vertexAI(),
+
+    // the local vector store requires an embedder to translate from text to vector
+    devLocalVectorstore([
+      {
+        indexName: "menuQA",
+        embedder: textEmbedding004,
+      },
+    ]),
+  ],
+  model: gemini15Flash,
+});
+
+export const menuSuggestionFlow = ai.defineFlow(
+  {
+    name: "menuSuggestionFlow",
+    inputSchema: z.string(),
+    outputSchema: z.string(),
+  },
+  async (restaurantTheme) => {
+    const { text } = await ai.generate({
+      model: gemini15Flash,
+      prompt: `Invent a menu item for a ${restaurantTheme} themed restaurant.`,
+      output: { schema: MenuItemSchema },
+    });
+    return text;
+  }
+);
 
 export const indexMenu = ai.defineFlow(
   {
@@ -51,42 +102,30 @@ export const indexMenu = ai.defineFlow(
   }
 );
 
-const ai = genkit({
-  plugins: [
-    googleAI(),
-    // vertexAI provides the textEmbedding004 embedder
-    vertexAI(),
-
-    // the local vector store requires an embedder to translate from text to vector
-    devLocalVectorstore([
-      {
-        indexName: "menuQA",
-        embedder: textEmbedding004,
-      },
-    ]),
-  ],
-  model: gemini15Flash,
-});
-
-const MenuItemSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  calories: z.number(),
-  allergens: z.array(z.string()),
-});
-
-export const menuSuggestionFlow = ai.defineFlow(
-  {
-    name: "menuSuggestionFlow",
-    inputSchema: z.string(),
-    outputSchema: z.string(),
-  },
-  async (restaurantTheme) => {
-    const { text } = await ai.generate({
-      model: gemini15Flash,
-      prompt: `Invent a menu item for a ${restaurantTheme} themed restaurant.`,
-      output: { schema: MenuItemSchema },
+export const menuQAFlow = ai.defineFlow(
+  { name: "menuQA", inputSchema: z.string(), outputSchema: z.string() },
+  async (input: string) => {
+    // retrieve relevant documents
+    const docs = await ai.retrieve({
+      retriever: menuRetriever,
+      query: input,
+      options: { k: 3 },
     });
+
+    // generate a response
+    const { text } = await ai.generate({
+      prompt: `
+You are acting as a helpful AI assistant that can answer 
+questions about the food available on the menu at Genkit Grub Pub.
+
+Use only the context provided to answer the question.
+If you don't know, do not make up an answer.
+Do not add or change items on the menu.
+
+Question: ${input}`,
+      docs,
+    });
+
     return text;
   }
 );
